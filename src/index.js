605dec552bc13f8a2cee913eca91f3ac59f249fb
AnnaIstochnikova <anya.istochnikova@gmail.com>
2023-09-01 09:30:33 +0200

    delete old js, create new js with axios

--- src/index.js
+++ src/index.js
@@ -1,93 +1,49 @@
 'use strict';
 
+import axios from 'axios';
+//import SlimSelect from 'slim-select';
+
+const options = {};
 const catInfo = document.querySelector('.cat-info');
 const breedSelect = document.querySelector('.breed-select');
-// import SlimSelect from 'slim-select';
 // new SlimSelect({
 //   select: breedSelect,
 // });
 
-const url = `https://api.thecatapi.com/v1/images/search?breed_ids=`;
-const api_key =
+axios.defaults.headers.common['x-api-key'] =
   'live_aBuxFCpJ1glwoKQiqCgrRj9d4CjKT3MTHnjKCwr34PtLohAh6jObeB2qr0uwjO10';
-
-let selectedOption = [];
-
-breedSelect.addEventListener('change', function handleChange(event) {
-  const selectedBreed = event.target.value;
-  selectedOption.push(selectedBreed);
-
-  console.log(selectedOption);
-  console.log(selectedOption[0]);
-  //console.log(renderCatBox(selectedOption[0]));
-  //renderSelect();
-  fetchCats()
-    .then(cats => renderSelect(cats))
-    .catch(error => console.log(error));
-});
-breedSelect.dispatchEvent(new Event('change'));
-
-function fetchCats(catId) {
-  return fetch(`${url}${selectedOption[0]}`, {
-    headers: {
-      'x-api-key': api_key,
-    },
+axios
+  .get('https://api.thecatapi.com/v1/breeds', options)
+  .then(response => {
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error(response.status);
+    }
   })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
+  .then(data => {
+    console.log(data);
+    renderSelect(data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+function renderSelect(cats) {
+  console.log(cats);
+  cats.forEach(cat => {
+    const addNameToSelect = `<option value="${cat.id}">${cat.name}</option>`;
+    breedSelect.insertAdjacentHTML('afterbegin', addNameToSelect);
+    // console.log(cat.name);
+  });
+
+  breedSelect.addEventListener('change', function handleChange(event) {
+    const selectedBreed = event.target.value;
+
+    cats.forEach(cat => {
+      if (selectedBreed !== null || selectedBreed === cat.name) {
+        console.log(cat.name);
       }
-      return response.json();
-    })
-    .then(data => {
-      breedSelect.append(...data.map(renderSelect));
     });
+  });
 }
-function renderSelect(cat) {
-  const addNameToSelect = `<option value="${cat.id}">${cat.name}</option>`;
-  breedSelect.insertAdjacentHTML('afterbegin', addNameToSelect);
-
-  function renderCatBox(cat) {
-    //cat.name = selectedOption[0];
-    console.log(cat);
-    const addCat = `<div class="box__cat"><img
-    class="cat__image"
-    src="${cat.image.url}"
-    alt="${cat.name}"
-  />
-  <div class="box__description"><h2>${cat.name}</h2>
-  <p>${cat.description}</p>
-  <p><h3>Temperament:</h3> ${cat.temperament}</p>
-  </div>
-  </>
-  </div>`;
-    catInfo.insertAdjacentHTML('afterbegin', addCat);
-
-    // const addNameToSelect = `<option value="${cat.name}">${cat.name}</option>`;
-    // breedSelect.insertAdjacentHTML('afterbegin', addNameToSelect);
-    // console.log(addCat);
-  }
-  renderCatBox(cat);
-}
-console.log(selectedOption[0]);
-
-// function renderCatBox(cat) {
-//   //cat.name = selectedOption[0];
-//   console.log(cat);
-//   const addCat = `<div class="box__cat"><img
-//   class="cat__image"
-//   src="${cat.image.url}"
-//   alt="${cat.name}"
-// />
-// <div class="box__description"><h2>${cat.name}</h2>
-// <p>${cat.description}</p>
-// <p><h3>Temperament:</h3> ${cat.temperament}</p>
-// </div>
-// </>
-// </div>`;
-//   catInfo.insertAdjacentHTML('afterbegin', addCat);
-
-//   const addNameToSelect = `<option value="${cat.name}">${cat.name}</option>`;
-//   breedSelect.insertAdjacentHTML('afterbegin', addNameToSelect);
-//   console.log(addCat);
-// }
